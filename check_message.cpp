b7a839681ea758d8f7097d1a0aa1fa00c90f78dd
rafnocerino <56391318+rafnocerino@users.noreply.github.com>
2020-05-13 18:40:00 +0200

    added some functions

--- check_message.cpp
+++ check_message.cpp
@@ -1,4 +1,5 @@
 #include "check_message.h"
+
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,6 +9,8 @@
 
 #include "protocol_constant.h"
 using namespace std;
+
+//aggiunte check challenge unavailable e start-->
 
 bool check_login(unsigned char* message, unsigned int messageLength,uint8_t* seqNum,char* username) {
     uint8_t actualOpcode;
@@ -23,7 +26,7 @@ bool check_login(unsigned char* message, unsigned int messageLength,uint8_t* seq
     return true;
 }
 
-bool check_ack(int socket, unsigned char* buffer, unsigned int messageLength, uint8_t exp_opcode, uint8_t exp_seq_numb) {
+bool check_ack(int socket, unsigned char* buffer, int messageLength, uint8_t exp_opcode, uint8_t exp_seq_numb) {
     uint8_t rcv_seq_numb;
     uint8_t rcv_opcode;
     int pos = 0;
@@ -47,47 +50,87 @@ bool check_ack(int socket, unsigned char* buffer, unsigned int messageLength, ui
     return true;
 }
 
-bool check_challengeRequest(int socket, unsigned char* buffer, unsigned int messageLength, uint8_t exp_opcode, uint8_t exp_seq_numb,
-                            unsigned char* challenging_user, int& challenge_id,uint8_t& rcv_seq_numb) {
-    
+bool check_challengeRequest(int socket, unsigned char* buffer, int messageLength, uint8_t exp_opcode, uint8_t exp_seq_numb,
+                            unsigned char* challenging_user, int& challenge_id, uint8_t& rcv_seq_numb) {
     uint8_t rcv_opcode;
     int pos = 0;
-	uint8_t data_len;
+    uint8_t data_len;
+    uint8_t seq, id;
 
     memcpy(&rcv_opcode, buffer, sizeof(rcv_opcode));
     pos += sizeof(rcv_opcode);
 
-    memcpy(&rcv_seq_numb, buffer + pos, sizeof(rcv_seq_numb));
-    pos += sizeof(rcv_seq_numb);
+    memcpy(&seq, buffer + pos, sizeof(seq));
+    pos += sizeof(seq);
+    rcv_seq_numb = seq;
 
-	memcpy(&challenge_id,buffer+pos,sizeof(challenge_id));
-	pos+=sizeof(challenge_id);
+    memcpy(&id, buffer + pos, sizeof(id));
+    pos += sizeof(id);
+    challenge_id = id;
 
-	memcpy(&data_len,buffer+pos,SIZE_LEN);
-	pos+=sizeof(data_len);
-    
-	strcpy((char*)challenging_user,(char*)buffer+pos);
-    pos+=strlen((char*)challenging_user)+1;
+    memcpy(&data_len, buffer + pos, SIZE_LEN);
+    pos += sizeof(data_len);
 
-	strtok((char*)challenging_user,";");
-    int flush_len =strlen((char*)challenging_user)+1;
-    memset(challenging_user+flush_len,0,255-flush_len);
+    strcpy((char*)challenging_user, (char*)buffer + pos);
+    pos += strlen((char*)challenging_user) + 1;
 
-	if (rcv_opcode == OPCODE_MALFORMED_MEX) {
+    strtok((char*)challenging_user, ";");
+    int flush_len = strlen((char*)challenging_user) + 1;
+    memset(challenging_user + flush_len, 0, 255 - flush_len);
+
+    if (rcv_opcode == OPCODE_MALFORMED_MEX) {
         // this means that the msg that i sent was modified during the forwarding
         close(socket);
         exit(-1);
     }
 
-	if(rcv_opcode != exp_opcode){
-		return false;
-	}
-	
-	if(data_len!=messageLength+SIZE_OPCODE+SIZE_SEQNUMBER+SIZE_CHALLENGE_NUMBER+SIZE_LEN+1){
-		return false;
-	}
+    if (rcv_opcode != exp_opcode) {
+        return false;
+    }
 
-	return true;
+    if (data_len != messageLength + SIZE_OPCODE + SIZE_SEQNUMBER + SIZE_CHALLENGE_NUMBER + SIZE_LEN + 1) {
+        return false;
+    }
+
+    return true;
+}
+
+bool check_challenge_Unavailable(int socket, unsigned char* buffer, int messageLength, uint8_t exp_seq_numb, uint8_t& rcv_seq_numb) {
+    uint8_t rcv_opcode;
+    uint8_t seq;
+    int pos = 0;
+
+    memcpy(&rcv_opcode, buffer, SIZE_OPCODE);
+    pos += SIZE_OPCODE;
+    memcpy(&seq, buffer + pos, SIZE_SEQNUMBER);
+    pos += SIZE_SEQNUMBER;
+    rcv_seq_numb = seq;
+
+    if (rcv_opcode == OPCODE_MALFORMED_MEX) {
+        close(socket);
+        exit(-1);
+    }
+    if ((rcv_opcode != OPCODE_CHALLENGE_UNAVAILABLE) || (exp_seq_numb != rcv_seq_numb)) {
+        return false;
+    }
+
+    return true;
+}
+
+bool check_challengeStart(int socket,unsigned char* buffer, int messageLength,uint8_t exp_seq_numb,uint8_t& recv_seq_numb,unsigned char* ip,unsigned char* adv_pubkey){
+
+    uint8_t rcv_opcode;
+    uint8_t seq;
+
+    //da terminare
 
 }
 
+bool check_message(uint8_t desiredOpcode, unsigned char* message, unsigned int messageLength, int desiredSequenceNumber) {
+    switch (desiredOpcode) {
+        case OPCODE_LOGIN: {
+            return check_message_login(message, messageLength);
+            break;
+        }
+    }
+}
--- check_message.h
+++ check_message.h
@@ -1,14 +1,14 @@
-#include <unistd.h>
+#include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 #include <sys/types.h>
-#include <stdint.h>
+#include <unistd.h>
 using namespace std;
 
-bool check_login(unsigned char* message, unsigned int messageLength,uint8_t* seqNum,char* username); 
-
-bool check_ack(int socket,unsigned char* buffer,unsigned int messageLength,uint8_t exp_opcode,uint8_t exp_seq_numb);
+bool check_challengeStart(int socket,unsigned char* buffer, int messageLength,uint8_t exp_seq_numb,uint8_t& recv_seq_numb,unsigned char* ip,unsigned char* adv_pubkey);
+bool check_challenge_Unavailable(int socket, unsigned char* buffer, int messageLength, uint8_t exp_seq_numb, uint8_t& rcv_seq_numb);
+bool check_ack(int socket, unsigned char* buffer, unsigned int messageLength, uint8_t exp_opcode, uint8_t exp_seq_numb);
 bool check_challengeRequest(int socket, unsigned char* buffer, unsigned int messageLength, uint8_t exp_opcode, uint8_t exp_seq_numb,
-                            unsigned char* challenging_user, int& challenge_id,uint8_t& rcv_seq_numb);
-
+                            unsigned char* challenging_user, int& challenge_id, uint8_t& rcv_seq_numb);
+bool check_login(unsigned char* message, unsigned int messageLength,uint8_t* seqNum,char* username); 
