b7d271de5fab4152a73a80afeddc0da51b3e659a
RiccardoXe <56175472+RiccardoXe@users.noreply.github.com>
2020-07-14 00:31:18 +0200

    Sign and Verification functions

---
+++ digital_signature.cpp
@@ -0,0 +1,127 @@
+#include <iostream> 
+#include <string>
+#include <stdio.h> // for fopen(), etc.
+#include <limits.h> // for INT_MAX
+#include <string.h> // for memset()
+#include <openssl/evp.h>
+#include <openssl/pem.h>
+
+using namespace std;
+
+struct signature{
+	unsigned char* sign_buf;
+	long int len;
+};
+
+
+unsigned char* signMsg(unsigned char* userName, unsigned char* msg_to_sign,struct signature* sign){
+
+   int ret; // used for return values
+
+   // read my private key file from keyboard:
+   string prvkey_file_name="prvkey";
+   prvkey_file_name+=reinterpret_cast<const char*>(userName);
+   prvkey_file_name+=".pem";
+
+   // load my private key:
+   FILE* prvkey_file = fopen(prvkey_file_name.c_str(), "r");
+   if(!prvkey_file){ cerr << "Error: cannot open file '" << prvkey_file_name << "' (missing?)\n"; exit(1); }
+   EVP_PKEY* prvkey = PEM_read_PrivateKey(prvkey_file, NULL, NULL, NULL);
+   fclose(prvkey_file);
+   if(!prvkey){ cerr << "Error: PEM_read_PrivateKey returned NULL\n"; exit(1); }
+
+   // 
+   const EVP_MD* md = EVP_sha256();
+   EVP_MD_CTX* md_ctx = EVP_MD_CTX_new();
+   if(!md_ctx){ cerr << "Error: EVP_MD_CTX_new returned NULL\n"; exit(1); }
+   // allocate buffer for signature:
+   unsigned char* sgnt_buf = (unsigned char*)malloc(EVP_PKEY_size(prvkey));
+   if(!sgnt_buf) { cerr << "Error: malloc returned NULL (signature too big?)\n"; exit(1); }
+   
+   // sign the plaintext:
+   // (perform a single update on the whole plaintext, 
+   // assuming that the plaintext is not huge)
+   ret = EVP_SignInit(md_ctx, md);
+   if(ret == 0){ cerr << "Error: EVP_SignInit returned " << ret << "\n"; exit(1); }
+   ret = EVP_SignUpdate(md_ctx, msg_to_sign, strlen((const char*)msg_to_sign));
+   if(ret == 0){ cerr << "Error: EVP_SignUpdate returned " << ret << "\n"; exit(1); }
+   unsigned int sgnt_size;
+   ret = EVP_SignFinal(md_ctx, sgnt_buf, &sgnt_size, prvkey);
+   if(ret == 0){ cerr << "Error: EVP_SignFinal returned " << ret << "\n"; exit(1); }
+   //datastructure init
+   sign->sign_buf= sgnt_buf;
+   sign->len=sgnt_size;
+   // delete the digest and the private key from memory:
+   EVP_MD_CTX_free(md_ctx);
+   EVP_PKEY_free(prvkey);
+   BIO_dump_fp(stdout,(const char*)sgnt_buf, strlen((const char*)sgnt_buf)+1);
+   //BIO_dump_fp(stdout,(const char*)msg_to_sign, strlen((const char*)msg_to_sign)+1);
+    cout<<sgnt_size<<endl;
+   
+
+}
+
+bool verifySignMsg(unsigned char* userName, unsigned char* text, struct signature* sign){
+   int ret; // used for return values
+
+   // read the peer's public key file from keyboard:
+   string pubkey_file_name="pubkey";
+   pubkey_file_name+=reinterpret_cast<const char*>(userName);
+   pubkey_file_name+=".pem";
+
+   // load the peer's public key:
+   FILE* pubkey_file = fopen(pubkey_file_name.c_str(), "r");
+   if(!pubkey_file){ cerr << "Error: cannot open file '" << pubkey_file_name << "' (missing?)\n"; exit(1); }
+   EVP_PKEY* pubkey = PEM_read_PUBKEY(pubkey_file, NULL, NULL, NULL);
+   fclose(pubkey_file);
+   if(!pubkey){ cerr << "Error: PEM_read_PUBKEY returned NULL\n"; exit(1); }
+
+   unsigned char* clear_buf = text;
+   unsigned char* sgnt_buf = sign->sign_buf;
+   long int clear_size=strlen((const char*)clear_buf);
+   long int sgnt_size=sign->len;
+   cout<<"Autenticazione"<<endl;
+   cout<<clear_buf<<endl;
+   cout<<sgnt_buf<<endl;
+   cout<<clear_size<<endl;
+   cout<<sgnt_size<<endl;
+   // declare some useful variables:
+   const EVP_MD* md = EVP_sha256();
+
+   // create the signature context:
+   EVP_MD_CTX* md_ctx = EVP_MD_CTX_new();
+   if(!md_ctx){ cerr << "Error: EVP_MD_CTX_new returned NULL\n"; exit(1); }
+
+   // verify the plaintext:
+   // (perform a single update on the whole plaintext, 
+   // assuming that the plaintext is not huge)
+   ret = EVP_VerifyInit(md_ctx, md);
+   if(ret == 0){ cerr << "Error: EVP_VerifyInit returned " << ret << "\n"; exit(1); }
+   ret = EVP_VerifyUpdate(md_ctx, clear_buf, clear_size);  
+   if(ret == 0){ cerr << "Error: EVP_VerifyUpdate returned " << ret << "\n"; exit(1); }
+   ret = EVP_VerifyFinal(md_ctx, sgnt_buf, sgnt_size, pubkey);
+   if(ret != 1){ // it is 0 if invalid signature, -1 if some other error, 1 if success.
+      cerr << "Error: EVP_VerifyFinal returned " << ret << " (invalid signature?)\n";
+      // deallocate buffers:
+      return false;
+   }
+   cout<<"QIO"<<endl;
+   //free(clear_buf);
+   //free(sgnt_buf);
+   EVP_PKEY_free(pubkey);
+   EVP_MD_CTX_free(md_ctx);
+   return true;
+}
+
+int main() {
+	struct signature sign;
+	unsigned char name[]="A";
+	unsigned char msg[] ="Ciaosdfsdfsdfsdfsdfadsfasdfsdsda";
+	signMsg(name,msg,&sign);
+	//*sign.sign_buf='A';
+	if(verifySignMsg(name,msg,&sign))
+		cout<<"Firma autenticata con SUCCESSO"<<endl;
+	else
+		cout<<"Firma NON AUTENTICA"<<endl;
+	return 0;
+}
