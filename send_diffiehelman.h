639619ced421ea1236867217d8941658fb85bb1d
rafnocerino <56391318+rafnocerino@users.noreply.github.com>
2020-12-20 19:42:34 +0100

    Delete send_diffiehelman.h

--- send_diffiehelman.h
+++
@@ -1,4 +0,0 @@
-void send_dh(int socket, struct sockaddr_in* sv_addr, EVP_PKEY* dhsigned, int len_dhsigned
-                           uint8_t seq_numb);
-void wait_ACK(int sd, sockaddr_in* sock, uint8_t sq_numb);
-void wait_dh(int sd, sockaddr_in* sock, uint8_t sq_numb, EVP_PKEY* param);
